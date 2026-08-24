a535e8f8201672d57b8a7f79b4369574ae5f4337
Lucas Oliveira <lima1301lucas@gmail.com>
2026-08-23 21:08:17 -0300

    feat: cria DTOs de Tarefa, Categoria, Prioridade, Status e Usuario

---
+++ DTOs/Tarefa/Update.cs
@@ -0,0 +1,14 @@
+﻿namespace ToDo.DTOs.Tarefa
+{
+    public class Update
+    {
+        public record UpdateTarefaDto(
+            string Titulo,
+            string? Descricao,
+            DateTime DataVencimento,
+            int CategoriaId,
+            int PrioridadeId,
+            int StatusId
+        );
+    }
+}
---
+++ DTOs/Usuario/Response.cs
@@ -0,0 +1,13 @@
+﻿namespace ToDo.DTOs.Usuario
+{
+    public class Response
+    {
+        public record UsuarioResponseDto(
+            int Id,
+            string PrimeiroNome,
+            string Sobrenome,
+            string Username,
+            string Email
+        );
+    }
+}
